5adcc26de1edcb85c36034fdbaa6c9293efccf20
aaronkim218 <218adk@gmail.com>
2025-09-03 16:13:13 -0400

    use hash router for gh pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider";
 import Portfolio from "./pages/Portfolio";
 import More from "./pages/More";
